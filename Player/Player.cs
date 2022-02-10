bc88e1138a19b32f7a60f08ba832f56c31172ef8
Leyla Hunn <leyla.hunn@outlook.com>
2022-02-10 15:22:38 +0000

    Added asset files for player and world

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,14 +3,6 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	public override void _Process(float delta) 
-	{
-		var direction = Vector3.Zero;
-		
-		if(Input.IsActionPressed("move_right")) {
-			direction.x += 1f;
-		}
-	}
 	// Declare member variables here. Examples:
 	// private int a = 2;
 	// private string b = "text";
@@ -22,8 +14,12 @@ public class Player : KinematicBody2D
 	}
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+  public override void _Process(float delta)
+  	{
+        var direction = Vector3.Zero;
+		
+		if(Input.IsActionPressed("move_right")) {
+			direction.x += 1f;
+		}
+  	}
 }
